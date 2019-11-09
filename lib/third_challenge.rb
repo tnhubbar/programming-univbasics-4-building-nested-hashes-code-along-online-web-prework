4ddf53089f089ae4e483b43102996897724e692e
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-09 13:23:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,14 +1,11 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
+      :patriarch => {:name => "Lord Montague", :age => "53"
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {:name => "Lady Montague", :age => "54"
         },
-      :hero => {
-        #your key/value pairs here
+      :hero => {:name => "Romeo", :name => "15"
         },
       :hero_friends => []
    }, 
